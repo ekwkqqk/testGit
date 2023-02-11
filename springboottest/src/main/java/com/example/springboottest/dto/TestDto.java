fe7d23d7bc6ba6d0a21419bfccd231fe903d9a0f
ekwkqqk <ekwkqqk@gmail.com>
2023-02-11 14:22:05 +0900

    로그 파일 생성 위치 및 설정

---
+++ springboottest/src/main/java/com/example/springboottest/dto/TestDto.java
@@ -0,0 +1,25 @@
+package com.example.springboottest.dto;
+
+public class TestDto {
+    
+    private String id;
+    private String createUser;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreateUser() {
+        return this.createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+
+}
