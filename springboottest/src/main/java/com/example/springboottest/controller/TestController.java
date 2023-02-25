bfd8c59054a71ebac92ba124d504f88c952b2578
ekwkqqk <ekwkqqk@gmail.com>
2023-02-25 15:52:00 +0900

    스프링 다국어 설정

--- springboottest/src/main/java/com/example/springboottest/controller/TestController.java
+++ springboottest/src/main/java/com/example/springboottest/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.example.springboottest.controller;
 
 import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.springboottest.dto.TestDto;
 import com.example.springboottest.entity.MembersEntity;
 import com.example.springboottest.mapper.IMembersDao;
+import com.example.springboottest.service.ExcelService;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +25,12 @@ public class TestController {
 
     @Autowired
     private IMembersDao iMembersDao;
+
+    @Autowired
+    private ExcelService excelService;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(value="/helloWorld")
     public String helloWorld() {
@@ -29,5 +41,20 @@ public class TestController {
     public List<MembersEntity> test() {
         List<MembersEntity> listMembers = iMembersDao.listMembers();
         return listMembers;
+    }
+
+    @GetMapping(value="/excel")
+    public void excel(HttpServletResponse response) throws Exception {
+        excelService.selectExcelList(response);
+    }
+
+    @GetMapping(value="/i18n")
+    public String i18n() {
+        System.out.println(Locale.US);
+        System.out.println(Locale.KOREA);
+        System.out.println(Locale.KOREAN);
+        System.out.println("en_US : " + messageSource.getMessage("enter", null, Locale.US));
+        System.out.println("ko_KR : " + messageSource.getMessage("enter", null, null));
+        return "확인";
     }
 }
